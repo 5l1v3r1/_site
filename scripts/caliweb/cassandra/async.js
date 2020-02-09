81b26705fadcbfb84dc5c3197b9927f10129771d
cali <42573065+7pub@users.noreply.github.com>
2020-02-09 19:01:32 +0100

    Update async.js

--- scripts/caliweb/cassandra/async.js
+++ scripts/caliweb/cassandra/async.js
@@ -13,3 +13,18 @@ function PasswordShowHide(button) {
   if (password.type == "password") { button.innerHTML = '<i id="fa-eye" class="fa fa-eye"></i>'; password.type = "text"; }
   else { button.innerHTML = '<i id="fa-eye" class="fa fa-eye-slash"></i>'; password.type = "password"; }
 }
+
+$(document).ready(function () {   //Vid
+  $("#500").on("click", function (e) {
+    e.preventDefault();
+    $("#k18mj6IACB").modal({
+      backdrop: "static",       //remove ability to close modal with click
+      keyboard: false,          //remove option to close with keyboard
+      show: true                //Display loader!
+    });
+    setTimeout(function () {
+      $("#k18mj6IACB").modal("hide");
+      $('#cfSZPxbvup').modal();
+    }, 500);
+  });
+});
